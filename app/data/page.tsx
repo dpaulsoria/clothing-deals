f3661d1c52de62537072dbc53f062c90741dae86
Paul Soria <dpaulsoria@gmail.com>
2024-08-20 16:47:22 -0500

    Unused

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -5,38 +5,13 @@ import { motion } from 'framer-motion';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 export default function GraficosPage() {
-  // const [statistics, setStatistics] = useState<Statistics[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [selectedStatistic, setSelectedStatistic] = useState<Statistics | null>(
-  //   null
-  // );
+
   const [page, setPage] = useState(1);
   const [totalItems, setTotalItems] = useState(0);
   const limit = 10; // Número de elementos por página
-
-  // const fetchStatistics = useCallback(async () => {
-  //   try {
-  //     setLoading(true);
-  //     const response = (await MapService.getAllStatisticsByUserId(
-  //       user_id,
-  //       page,
-  //       limit
-  //     )) as unknown as { result: Statistics[]; extra: number };
-  //     if (response) {
-  //       setStatistics(response.result);
-  //       setTotalItems(response.extra);
-  //     } else {
-  //       setError('Error al obtener las estadísticas');
-  //     }
-  //   } catch (err) {
-  //     console.error('Error al obtener las estadísticas:', err);
-  //     setError(err instanceof Error ? err.message : 'Error desconocido');
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }, [user_id, page, limit]);
 
   const handleModalClose = () => {
     setIsModalOpen(false);
@@ -80,40 +55,6 @@ export default function GraficosPage() {
           Gráficos Estadísticos Guardados
         </motion.h1>
       </div>
-
-      {/*<motion.div*/}
-      {/*  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"*/}
-      {/*  initial={{ opacity: 0, y: 10 }}*/}
-      {/*  animate={{ opacity: 1, y: 0 }}*/}
-      {/*  transition={{ duration: 0.5, delay: 0.1 }}*/}
-      {/*>*/}
-      {/*  {statistics?.map((statistic: Statistics) => (*/}
-      {/*    <motion.div*/}
-      {/*      key={statistic.id}*/}
-      {/*      className="bg-white border border-gray-200 p-6 rounded-xl shadow-lg flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300 cursor-pointer"*/}
-      {/*      whileHover={{ scale: 1.02 }}*/}
-      {/*      transition={{ duration: 0.3 }}*/}
-      {/*      onClick={() => handleStatisticClick(statistic)}*/}
-      {/*    >*/}
-      {/*      <div className="mb-4">*/}
-      {/*        <h2 className="text-xl font-semibold mb-2 text-indigo-600 flex items-center">*/}
-      {/*          <FaChartBar className="mr-2" />*/}
-      {/*          {statistic.name}*/}
-      {/*        </h2>*/}
-      {/*        <p className="text-gray-500 text-sm">*/}
-      {/*          Usuario ID: {statistic.user_id}*/}
-      {/*        </p>*/}
-      {/*        <p className="text-gray-500 text-sm">*/}
-      {/*          Inicio: {new Date(statistic.timeline_init).toLocaleDateString()}*/}
-      {/*        </p>*/}
-      {/*        <p className="text-gray-500 text-sm">*/}
-      {/*          Fin: {new Date(statistic.timeline_finish).toLocaleDateString()}*/}
-      {/*        </p>*/}
-      {/*      </div>*/}
-      {/*    </motion.div>*/}
-      {/*  ))}*/}
-      {/*</motion.div>*/}
-
       <div className="flex justify-between items-center mt-4">
         <button
           onClick={handlePreviousPage}
@@ -133,36 +74,6 @@ export default function GraficosPage() {
           Siguiente <FaArrowRight className="ml-2" />
         </button>
       </div>
-
-      {/*{isModalOpen && selectedStatistic && (*/}
-      {/*  <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">*/}
-      {/*    <div className="bg-white w-full max-w-md rounded-lg shadow-lg p-6">*/}
-      {/*      <h3 className="text-2xl font-semibold text-gray-800 mb-4">*/}
-      {/*        Editar Nombre de la Estadística*/}
-      {/*      </h3>*/}
-      {/*      <input*/}
-      {/*        type="text"*/}
-      {/*        value={newName}*/}
-      {/*        onChange={(e) => setNewName(e.target.value)}*/}
-      {/*        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4"*/}
-      {/*      />*/}
-      {/*      <div className="flex justify-end space-x-4">*/}
-      {/*        <button*/}
-      {/*          onClick={handleModalClose}*/}
-      {/*          className="bg-gray-200 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-300"*/}
-      {/*        >*/}
-      {/*          Cancelar*/}
-      {/*        </button>*/}
-      {/*        <button*/}
-      {/*          onClick={handleUpdateStatistic}*/}
-      {/*          className="bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-600"*/}
-      {/*        >*/}
-      {/*          Guardar*/}
-      {/*        </button>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*)}*/}
     </div>
   );
 }
