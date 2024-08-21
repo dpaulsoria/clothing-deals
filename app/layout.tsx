4839986b8e9f75a883b0efb5b7d38483a430a650
Paul Soria <dpaulsoria@gmail.com>
2024-08-21 13:06:16 -0500

    Deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,19 +39,19 @@ export default function RootLayout({
         {pathname !== '/login' && (
           <header className="sticky top-0 z-[9999] bg-white shadow-md">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-              <Link
-                href="/"
-                className="transform transition-transform duration-300 hover:scale-105"
-              >
-                <GalapagosIcon className="fill-indigo-800" />
-              </Link>
+              {/*<Link*/}
+              {/*  href="/"*/}
+              {/*  className="transform transition-transform duration-300 hover:scale-105"*/}
+              {/*>*/}
+              {/*  <GalapagosIcon className="fill-indigo-800" />*/}
+              {/*</Link>*/}
               {/* Botón para generar los datos */}
-              <button
-                onClick={handleGenerateData}
-                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-              >
-                Generar Datos
-              </button>
+              {/*<button*/}
+              {/*  onClick={handleGenerateData}*/}
+              {/*  className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"*/}
+              {/*>*/}
+              {/*  Generar Datos*/}
+              {/*</button>*/}
               <nav className="flex items-center gap-6">
                 <Link
                   href="/about"
@@ -65,18 +65,18 @@ export default function RootLayout({
                 >
                   Dashboard
                 </Link>
-                <Link
-                  href="/stores"
-                  className="text-gray-700 hover:text-blue-600"
-                >
-                  Tiendas
-                </Link>
-                <Link
-                  href="/login"
-                  className="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700"
-                >
-                  Iniciar Sesión
-                </Link>
+                {/*<Link*/}
+                {/*  href="/stores"*/}
+                {/*  className="text-gray-700 hover:text-blue-600"*/}
+                {/*>*/}
+                {/*  Tiendas*/}
+                {/*</Link>*/}
+                {/*<Link*/}
+                {/*  href="/login"*/}
+                {/*  className="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700"*/}
+                {/*>*/}
+                {/*  Iniciar Sesión*/}
+                {/*</Link>*/}
               </nav>
             </div>
           </header>
